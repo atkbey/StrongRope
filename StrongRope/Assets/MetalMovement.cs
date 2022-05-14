514f3a98b5508e58207c2f6ff31f97d9956db4a7
atkbey <reploxs@gmail.com>
2022-05-14 17:02:31 +0300

    1.2

--- StrongRope/Assets/MetalMovement.cs
+++ StrongRope/Assets/MetalMovement.cs
@@ -2,23 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MetalMovement : MonoBehaviour
 {
 
+    public enum Taraf {
+        sol, sag
+    };
+
+     public Taraf taraf;
 
     float tmpSpeed;
 
+    float tmpSpeed2;
+
     public bool sagBasladi;
+
+    public bool solBasladi;
 
     [SerializeField] float speed;
 
+    [SerializeField] float speed_2;
+
 
     [SerializeField] float solSpeed;
+    
+    [SerializeField] float sagSpeed;
 
 
     public Rigidbody2D rigidbody2D;
 
-    int sayac;
+    int sayac,sayac_2;
 
     float time;
 
@@ -29,6 +43,9 @@ public class MetalMovement : MonoBehaviour
         rigidbody2D = GetComponent<Rigidbody2D>();
 
         tmpSpeed = speed;
+
+        tmpSpeed2 = speed_2;
+
     }
 
     // Update is called once per frame
@@ -51,9 +68,74 @@ public class MetalMovement : MonoBehaviour
         *
         *
         */
+        if (taraf==Taraf.sol)
+        {
+            transform.position += new Vector3(-solSpeed * Time.deltaTime, 0, 0);
+
+            if (sagBasladi && speed > 0)
+            {
+                transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+
+                speed -= 0.1f;
 
 
-        transform.position += new Vector3(-solSpeed * Time.deltaTime, 0, 0);
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                sagBasladi = true;
+
+
+                speed = tmpSpeed;
+
+                /*if (sayac < 3)
+                {
+                    sayac++;
+
+
+                    Vector3 a = new Vector3(0.01f, 0, 0);
+                    rigidbody2D.velocity = a * speed;
+
+
+                }
+                */
+
+            }
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                sayac = 0;
+
+            }
+
+        }
+
+        if (taraf ==Taraf.sag)
+        {
+            transform.position += new Vector3(sagSpeed * Time.deltaTime, 0, 0);
+
+            if (solBasladi && speed_2>0)
+            {
+                transform.position += new Vector3(-speed_2 * Time.deltaTime, 0, 0);
+                speed_2 -= 0.1f;
+            }
+
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                solBasladi = true;
+                speed_2 = tmpSpeed2;
+            }
+
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                sayac_2 = 0;
+
+            }
+
+        }
+
+
+        /*transform.position += new Vector3(-solSpeed * Time.deltaTime, 0, 0);
 
         if (sagBasladi &&  speed>0)
         {
@@ -82,14 +164,14 @@ public class MetalMovement : MonoBehaviour
 
                 
             }
-            */
+            
             
         }
         if (Input.GetKeyUp(KeyCode.A))
         {
             sayac = 0;
 
-        }
+        }*/
 
 
 
